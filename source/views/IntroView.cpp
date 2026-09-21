90c578db780bbc3befb9da1f9804da92c9b2c310
Gregory Munro <71084409+ggmini@users.noreply.github.com>
2026-09-22 00:05:30 +0200

    Center Text on IntroView (#611)
    Adjusted origin position so the text appears centered

--- source/views/IntroView.cpp
+++ source/views/IntroView.cpp
@@ -347,7 +347,7 @@ ViewState IntroView::update()
         animateText = true;
         REG_BLDCNT_SUB = BLEND_ALPHA | BLEND_SRC_BG3 | BLEND_DST_BG0 | BLEND_DST_BG1 | BLEND_DST_BACKDROP;
         REG_BLDALPHA_SUB = textAlpha | ((16 - textAlpha) << 8);
-        text->drawText("\xFF\x02\x01Press Any Button", 45, 80, TextColor::White);
+        text->drawText("\xFF\x02\x01Press Any Button", 35, 82, TextColor::White);
     }
 
     // setup blending for overlay
