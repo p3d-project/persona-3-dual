781441e7b13db5fb93618472f85c33667446494a
“David <barddro.art@gmail.com>
2026-09-25 04:34:02 +0000

    Fixed issue with const fields in save struct

--- source/types/SaveTypes.hpp
+++ source/types/SaveTypes.hpp
@@ -2,8 +2,8 @@
 
 struct Save
 {
-    const uint8_t majorVersion;
-    const uint8_t minorVersion;
+    uint8_t majorVersion;
+    uint8_t minorVersion;
     char lastName[32];
     char firstName[32];
 } __attribute__((packed));
